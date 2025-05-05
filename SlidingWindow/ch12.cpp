6344103a02f4c4e0784a92b343d0537880f17d79
sid2487 <sid454804@gmail.com>
2025-05-05 22:08:26 +0530

    did some more qs on sliding window

---
+++ SlidingWindow/ch12.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// rearrange array elements by sign in alterante positive and negative and array has even length
+// tc: O(2n) but sc: O( n) so its a brut solution
+vector<int> alternateElements(vector<int> &arr){
+    int n = arr.size();
+    vector<int> positive;
+    vector<int> negative;
+    for(int i=0; i<n/2; i++){
+        positive.push_back(arr[2*i]);
+        negative.push_back(arr[(2*i) + 1]);
+    }
+
+    vector<int> result;
+    int i = 0, j = 0;
+    bool turnPositive = true;
+
+    // Alternate between positive and negative
+    while (i < positive.size() && j < negative.size())
+    {
+        if (turnPositive)
+            result.push_back(positive[i++]);
+        else
+            result.push_back(negative[j++]);
+        turnPositive = !turnPositive;
+    }
+
+    // Add remaining positives
+    while (i < positive.size())
+        result.push_back(positive[i++]);
+
+    // Add remaining negatives
+    while (j < negative.size())
+        result.push_back(negative[j++]);
+
+    return result;
+}
+int main() {
+
+
+
+
+    return 0;
+}
